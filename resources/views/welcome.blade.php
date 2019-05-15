31283089eed8a89fcd5f5188970e48d6c9116434
ZakMos <z.zza@hotmail.com>
2019-05-15 13:46:36 +0200

    adjusting the course project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,32 @@
 @extends('layouts.master')
 @section('content')
-<div class="row mt-5">
-  <div class="col-md-12">
-    <h1>Control Structures</h1>
-    @if(true)
-      <p>This only displays if it is true</p>
-    @else
-      <p>This only displays if it is false</p>
-    @endif
-    <hr>
-    @for($i = 0; $i < 5; $i++)
-      <p>{{ $i + 1 }}. Iteration</p>
-    @endfor
-    <hr>
-    <h2>XSS</h2>
-    {{ "<script>alert('Hello');</script>" }}
-    {!! "<script>alert('Hello');</script>" !!}
-
+<div class="mt-5">
+  <div class="row">
+    <div class="col-md-12">
+      <p class="quote">The Beautiful Laravel</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h1 class="post-title">Learning Laravel</h1>
+      <p>This blog post will get you right on track with Laravel!</p>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-md-12">
+      <h1 class="post-title">The next Steps</h1>
+      <p>Understanding the Basics is great, but you need to be able to make the next steps.</p>
+      <p><a href="#">Read More...</a></p>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-md-12">
+      <h1 class="post-title">Laravel 5.3</h1>
+      <p>Though announced as a "minor relese", Laravel 5.3 ships with some very interesting additions and feautres.</p>
+      <p><a href="#">Read More...</a></p>
+    </div>
   </div>
 </div>
-
-
-  <!--  output Hello  -->
-  <!-- <p>{{ 2 == 2 ? "Hello" : "Does not equal" }}</p> -->
-
-  <!-- output Does not equal -->
-  <!-- <p>{{ 2 == 3 ? "Hello" : "Does not equal" }}</p> -->
-
-  <!-- output Hello  -->
-  <!-- <p>{{ "Hello"  }}</p> -->
-
 @endsection
