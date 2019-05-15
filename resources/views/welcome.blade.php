4b87ed71687ab99582c381f03e5c0b8184b08e7b
ZakMos <z.zza@hotmail.com>
2019-05-15 13:05:55 +0200

    add header with bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.master')
 @section('content')
-  <h1>Some Content</h1>
+<div class="row">
+  <div class="col-md-12">
+    <h1>Some Content</h1>
+    <p>{{ "Hello"  }}</p>
+  </div>
+</div>
+
 
   <!--  output Hello  -->
-  <p>{{ 2 == 2 ? "Hello" : "Does not equal" }}</p>
+  <!-- <p>{{ 2 == 2 ? "Hello" : "Does not equal" }}</p> -->
 
   <!-- output Does not equal -->
-  <p>{{ 2 == 3 ? "Hello" : "Does not equal" }}</p>
+  <!-- <p>{{ 2 == 3 ? "Hello" : "Does not equal" }}</p> -->
 
   <!-- output Hello  -->
-  <p>{{ "Hello"  }}</p>
+  <!-- <p>{{ "Hello"  }}</p> -->
 
 @endsection
