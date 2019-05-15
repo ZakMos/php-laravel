be394c7fd5d9cd18e70642a1e535f043e7eaf50d
ZakMos <z.zza@hotmail.com>
2019-05-15 12:46:03 +0200

    partials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.master')
 @section('content')
-<h1>Some Content</h1>
+  <h1>Some Content</h1>
+
+  <!--  output Hello  -->
+  <p>{{ 2 == 2 ? "Hello" : "Does not equal" }}</p>
+
+  <!-- output Does not equal -->
+  <p>{{ 2 == 3 ? "Hello" : "Does not equal" }}</p>
+
+  <!-- output Hello  -->
+  <p>{{ "Hello"  }}</p>
+
 @endsection
