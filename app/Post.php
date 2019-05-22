4a973c77b453c6757c41f989c62d51df943cf0f3
ZakMos <z.zza@hotmail.com>
2019-05-22 13:52:59 +0200

    Adding Method to Model

--- app/Post.php
+++ app/Post.php
@@ -1,5 +1,23 @@
 <?php
-
+namespace App;
 class Post {
-  
+  public function getPosts($session) {
+    if(!session->has('posts')){
+      $this->createDummyData($session);
+    }
+    return $session->get('posts');
+  }
+  private function createDummyData() {
+    $posts = [
+      [
+        'title' => 'Learining Laravel',
+        'content' => 'This blog post will get you right on track with Laravel!'
+      ],
+      [
+        'title' => 'Something else',
+        'content' => 'Some other content'
+      ]
+    ];
+    $session->put('posts', $posts);
+  }
 }
