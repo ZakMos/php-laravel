c45422db1642aabb7ae481dca149ee4e3421d07a
ZakMos <z.zza@hotmail.com>
2019-05-22 18:26:24 +0200

    Finishing the Post Model

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,29 @@ class Post {
     }
     return $session->get('posts');
   }
+
+public function getPost($session, $id) {
+  if (!$session->has('posts')) {
+    $this->createDummyData();
+  }
+  return $session->get('posts') [$id];
+}
+
+public function addPost($session, $title, $content) {
+  if(!$session->has('posts')) {
+    $this->createDummyData();
+  }
+  $posts = $session->get('posts');
+  array_push($posts, ['title' => $title, 'content' => $content]);
+  $session->put('posts', $posts);
+}
+
+public function editPost($session, $id, $title, $content) {
+  $posts = $session->get('posts');
+  $posts[$id] = ['title' => $title, 'content' => $content];
+  $session->put('posts', $posts);
+}
+
   private function createDummyData($session) {
     $posts = [
       [
