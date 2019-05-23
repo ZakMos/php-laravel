e53b4d7704ab586871df40ff4e28189038ebcd59
ZakMos <z.zza@hotmail.com>
2019-05-23 15:24:45 +0200

    Making a Model 'Filable'

--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+  protected $filable = ['title', 'content'];
+  
   public function getPosts($session)
   {
     if (!$session->has('posts')) {
