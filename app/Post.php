4d83481d8515b7bd4ee2a3182743431eb375ef7b
ZakMos <z.zza@hotmail.com>
2019-05-29 13:49:12 +0200

    Defining Many to Many

--- app/Post.php
+++ app/Post.php
@@ -9,6 +9,11 @@ class Post extends Model
 
   public function likes()
   {
-      return $this->hasMany('App\Like');
+      return $this->hasMany('App\Like', 'post_id');
+  }
+
+  public function tags()
+  {
+    return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id')->withTimestamps();
   }
 }
