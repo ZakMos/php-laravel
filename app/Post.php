7e13cde3b83a4837169205bd90140b5dec3898c6
ZakMos <z.zza@hotmail.com>
2019-06-02 20:06:45 +0200

    Using Accessors

--- app/Post.php
+++ app/Post.php
@@ -5,20 +5,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-  protected $fillable = ['title', 'content'];
+    protected $fillable = ['title', 'content'];
 
-  public function likes()
-  {
-      return $this->hasMany('App\Like', 'post_id');
-  }
+    public function likes()
+    {
+        return $this->hasMany('App\Like', 'post_id');
+    }
 
-  public function tags()
-  {
-      return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id')->withTimestamps();
-  }
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'post_tag', 'post_id', 'tag_id')->withTimestamps();
+    }
 
-  public function setTitleAttribute($value)
-  {
-      $this->attributes['title'] = strtolower($value);
-  }
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = strtolower($value);
+    }
+
+    // public function setContentAttribute($value)
+    // {
+    //     $this->attributes['content'] = strtoupper($value);
+    // }
+
+    public function getTitleAttribute($value)
+    {
+        return strtoupper($value);
+    }
 }
